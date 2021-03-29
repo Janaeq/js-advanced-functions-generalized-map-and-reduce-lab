81fe2ff5389cadf550ac1ba9ab9a850fd2277d24
Janae Quinones <janaemassa@gmail.com>
2021-03-29 15:20:27 -0400

    commit tree 9607d6eb0a304aed165e1f34ca3685b25e323e8e

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+function map(arr, func) {
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(func(arr[i]))
+    }
+    return newArr
+}
+
+function reduce(arr, func, start = null) {
+    if (start) {
+      arr.forEach((el) => {
+        start = func(el, start)
+      })
+    } else {
+      start = arr[0];
+      for (let i = 1; i < arr.length; i++) {
+        start = func(arr[i], start)
+      }
+    }
+    return start
+}
